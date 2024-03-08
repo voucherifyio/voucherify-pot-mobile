42e95e8e1fa8872be75c224320bccc0620c4a2d7
weronika-kurczyna <117282008+weronika-kurczyna@users.noreply.github.com>
2024-03-08 13:05:18 +0100

    Update navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,46 +5,50 @@ import {
     MdOutlineMap,
     MdStars,
 } from 'react-icons/md'
+import Link from 'next/link'
 
 const Navbar = () => {
+    const MENU_LIST = [
+        {
+            text: 'Home',
+            href: '/home',
+            icon: <MdHome color="gray" />,
+        },
+        {
+            text: 'Deals',
+            href: '/deals',
+            icon: <MdOutlineLocalOffer color="gray" />,
+        },
+        {
+            text: 'Card',
+            href: '/card',
+            icon: <MdCreditCard color="gray" />,
+        },
+        {
+            text: 'Map',
+            href: '/map',
+            icon: <MdOutlineMap color="gray" />,
+        },
+        {
+            text: 'Reward',
+            href: '/reward',
+            icon: <MdStars color="gray" />,
+        },
+    ]
     return (
         <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200">
             <div className="grid h-full max-w-screen-sm grid-cols-5 mx-auto font-medium">
-                <button
-                    type="button"
-                    className="inline-flex flex-col items-center justify-center px-5 "
-                >
-                    <MdHome color="gray" />
-                    <span className="text-sm text-gray-500  ">Home</span>
-                </button>
-                <button
-                    type="button"
-                    className="inline-flex flex-col items-center justify-center px-5 group"
-                >
-                    <MdOutlineLocalOffer color="gray" />
-                    <span className="text-sm text-gray-500 ">Deals</span>
-                </button>
-                <button
-                    type="button"
-                    className="inline-flex flex-col items-center justify-center px-5 group"
-                >
-                    <MdCreditCard color="gray" />
-                    <span className="text-sm text-gray-500 ">Card</span>
-                </button>
-                <button
-                    type="button"
-                    className="inline-flex flex-col items-center justify-center px-5 group"
-                >
-                    <MdOutlineMap color="gray" />
-                    <span className="text-sm text-gray-500">Map</span>
-                </button>
-                <button
-                    type="button"
-                    className="inline-flex flex-col items-center justify-center px-5 group"
-                >
-                    <MdStars color="gray" />
-                    <span className="text-sm text-gray-500">Rewards</span>
-                </button>
+                {MENU_LIST.map((item) => (
+                    <Link
+                        className="inline-flex flex-col items-center justify-center px-5 "
+                        href={item.href}
+                    >
+                        {item.icon}
+                        <span className="text-sm text-gray-500">
+                            {item.text}
+                        </span>
+                    </Link>
+                ))}
             </div>
         </div>
     )
