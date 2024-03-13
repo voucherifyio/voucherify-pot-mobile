4cb6f90457d9b40081b8ee4263cdd6d78ac17a1b
weronika-kurczyna <117282008+weronika-kurczyna@users.noreply.github.com>
2024-03-13 11:00:00 +0100

    Do not allow certain urls for not logged in cust

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,31 +3,31 @@ import { Metadata } from 'next'
 import SessionWrapper from './components/SessionWrapper'
 import Navbar from '@/app/components/navbar'
 import { inter } from '@/app/components/ui/fonts'
+import { useSession } from 'next-auth/react'
 
 export const metadata: Metadata = {
     title: {
-        template: '%s | Parkland',
-        default: 'Parkland',
+        template: '%s | Journie',
+        default: 'Journie',
     },
     description: 'Parkland POT',
 }
 
-export default function RootLayout({ children }: {
+export default function RootLayout({
+    children,
+}: {
     children: React.ReactNode
 }) {
     return (
         <SessionWrapper>
             <html lang="en">
-            <body
-                className={`${inter.className} mx-auto max-w-screen-sm antialiased`}
-            >
-            <Navbar />
-            {children}
-            </body>
+                <body
+                    className={`${inter.className} mx-auto max-w-screen-sm antialiased`}
+                >
+                    <Navbar />
+                    {children}
+                </body>
             </html>
         </SessionWrapper>
     )
 }
-
-
-
--- app/rewards/page.tsx
+++ app/rewards/page.tsx
@@ -1,11 +1,30 @@
+'use client'
 import JournieHeader from '@/app/components/journie-header/journie-header'
 import Rewards from '@/app/components/rewards/rewards'
+import { useRouter } from 'next/navigation'
+import { useSession } from 'next-auth/react'
+import { useEffect } from 'react'
 
 export default function RewardsPage() {
+    const router = useRouter()
+    const { data: session, status } = useSession()
+    useEffect(() => {
+        if (status === 'loading') {
+            return
+        }
+
+        if (!session || status !== 'authenticated') {
+            router.push('/')
+        }
+    }, [session, status, router])
     return (
-        <div className="h-screen items-center justify-center">
-            <JournieHeader headerText={'Journie REWARDS'} />
-            <Rewards />
-        </div>
+        <>
+            {status === 'authenticated' && (
+                <div className="h-screen items-center justify-center">
+                    <JournieHeader headerText={'Journie REWARDS'} />
+                    <Rewards />
+                </div>
+            )}
+        </>
     )
 }
