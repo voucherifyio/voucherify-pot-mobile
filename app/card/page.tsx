70419d2b202a55af8ef112ba96f29579939bd51c
weronika-kurczyna <117282008+weronika-kurczyna@users.noreply.github.com>
2024-03-13 18:58:21 +0100

    Create loyalty card component

--- app/card/page.tsx
+++ app/card/page.tsx
@@ -1,30 +1,15 @@
 'use client'
-import Image from 'next/image'
-import Button from '@/app/components/ui/atoms/button'
 import ActiveRewards from '@/app/components/active-rewards'
 import JournieHeader from '@/app/components/journie-header/journie-header'
 import { useRouter } from 'next/navigation'
 import { useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
-
-interface LoyaltyCardResponse {
-    loyaltyCard: {
-        code: string
-        barcode: {
-            id: string
-            url: string
-        }
-    }
-}
+import LoyaltyCard from '@/app/components/loyalty-card/loyalty-card'
 
 export default function CardPage() {
-    //todo show error
-    const [error, setError] = useState<undefined | string>(undefined)
-    const [cardUrl, setCardUrl] = useState<string>('')
-    const [cardNumber, setCardNumber] = useState<string>('')
-    const [codeCopied, setCodeCopied] = useState<boolean>(false)
     const router = useRouter()
     const { data: session, status } = useSession()
+    const customerId = session?.user?.id
 
     useEffect(() => {
         if (status === 'loading') {
@@ -33,47 +18,8 @@ export default function CardPage() {
         if (!session || status !== 'authenticated') {
             router.push('/')
         }
-        const fetchData = async () => {
-            const customerId = session?.user?.id
-            if (customerId) {
-                try {
-                    const res = await fetch(
-                        `/api/loyalty-card?customerId=${customerId}`,
-                        {
-                            method: 'GET',
-                            headers: { 'Content-Type': 'application/json' },
-                        }
-                    )
-                    const data: LoyaltyCardResponse = await res.json()
-
-                    setCardUrl(data.loyaltyCard.barcode.url)
-                    setCardNumber(data.loyaltyCard.code)
-                } catch (err) {
-                    if (err instanceof Error) {
-                        return setError(err.message)
-                    }
-                    return err
-                }
-            }
-        }
-        if (!cardUrl || !cardNumber) {
-            fetchData().catch(console.error)
-        }
     }, [status, router, session])
 
-    const handleCopy = async () => {
-        try {
-            setCodeCopied(false)
-            await navigator.clipboard.writeText(cardNumber)
-            setCodeCopied(true)
-            setTimeout(() => {
-                setCodeCopied(false)
-            }, 2000)
-        } catch (err) {
-            console.error('Unable to copy code')
-            setCodeCopied(false)
-        }
-    }
     return (
         <>
             {status === 'authenticated' && (
@@ -89,21 +35,7 @@ export default function CardPage() {
                                 points and redeem rewards.
                             </h4>
                         </header>
-                        {cardUrl && (
-                            <Image
-                                src={cardUrl}
-                                alt="loyaltyCardBarCode"
-                                width={390}
-                                height={106}
-                                className="max-w-auto"
-                            />
-                        )}
-                        <Button
-                            onClick={handleCopy}
-                            className="w-full my-4 bg-blue-background border border-blue-inputOutlineDefault"
-                        >
-                            {codeCopied ? 'Code copied!' : 'Copy number'}
-                        </Button>
+                        {customerId && <LoyaltyCard customerId={customerId} />}
                         <ActiveRewards />
                     </div>
                 </div>
