56c6c6c644c3ea687b3b45376eb5a613cd07457d
Patryk Smolarz <patryk.smolarz1@gmail.com>
2024-03-21 17:48:24 +0100

    check if customer exist

--- app/api/voucherify/qualifications/route.ts
+++ app/api/voucherify/qualifications/route.ts
@@ -1,8 +1,16 @@
 import { getVoucherify } from '@/voucherify/voucherify-config'
 import { getQualifications } from '@/voucherify/get-qualifications'
 import { CAMPAIGNS } from '@/enum/campaigns'
-export async function POST(req: Request) {
+import { NextRequest } from 'next/server'
+export async function POST(req: NextRequest) {
     const { customerId, scenario, customerMetadata } = await req.json()
+
+    if (!customerId) {
+        return Response.json(
+            { error: 'Customer does not exist.' },
+            { status: 400 }
+        )
+    }
 
     try {
         const qualifications = await getQualifications({
